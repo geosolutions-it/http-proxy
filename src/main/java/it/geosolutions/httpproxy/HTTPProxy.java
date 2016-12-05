dd53d0856715eefd9e726fa103500fc6b803b495
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2016-12-05 17:42:32 +0100

    removed unused import

--- src/main/java/it/geosolutions/httpproxy/HTTPProxy.java
+++ src/main/java/it/geosolutions/httpproxy/HTTPProxy.java
@@ -22,7 +22,6 @@ package it.geosolutions.httpproxy;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
